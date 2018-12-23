e7744b29e497c659f576b7f00cbc861233795086
sesevgen <sesevgen@gmail.com>
2018-12-23 12:47:33 -0800

    added cell division graphics

--- simulation.py
+++ simulation.py
@@ -5,17 +5,19 @@ import matplotlib.pyplot as plt
 
 # Simulation parameters
 # Velocity change per timestep
-dv = 0.5
+dv = 1
 
 # Maybe try Langevin motion?
 #m = 1
 #l = 0.1
 
 # Cell split rate
-split = 0.01
+# Not stochastic anymore.
+#split = 0.01
+split_age = 25
 
 # Death rate
-death = 0.01
+death = 0.05
 
 # Target pop
 pop_target = 100.0
@@ -35,14 +37,20 @@ np.set_printoptions(threshold=np.nan)
 
 #-------------------------------
 
-def add_gaussian(array,center):
+def add_gaussian(array,center,age):
 	x=np.linspace(-4,4,9)
 	y=np.linspace(-4,4,9)
 	x,y = np.meshgrid(x, y)
-	sigma_x = 2
-	sigma_y = 2
-	z = np.uint8((1/(2*np.pi*sigma_x*sigma_y) * np.exp(-(x**2/(2*sigma_x**2)
-	     + y**2/(2*sigma_y**2))))*2**12)
+	if age == split_age or age == 0 or age == 1:
+		sigma_x = 0.5*(age+split_age)/(2.0*split_age)
+		sigma_y = 2.0*(age+split_age)/(2.0*split_age)
+	else:
+		sigma_x = 2.0*(age+split_age)/(2.0*split_age)
+		sigma_y = 2.0*(age+split_age)/(2.0*split_age)
+	z = (1/(2*np.pi*sigma_x*sigma_y) * np.exp(-(x**2/(2*sigma_x**2)
+	     + y**2/(2*sigma_y**2))))*2**12*(age+split_age)/(2.0*split_age)
+	z = np.clip(z,0,255)
+	z = np.uint8(z)
 	
 	c_int = center.astype(int)	
 	x=[max(c_int[0]-4,0),min(c_int[0]+5,img_size)]
@@ -76,18 +84,23 @@ class Cell(object):
 	# Add imaging parameters
 	
 
-	def __init__(self,coords, lineage, unique_id):
+	def __init__(self,coords, lineage, unique_id,age=-1):
 		self.coords = coords
 		self.lineage = lineage
 		self.unique_id = unique_id
+		if age == -1:
+			self.age = int(np.random.rand()*10)
+		else:
+			self.age = age
 		
-		self.velocity = (np.random.rand(3)-0.5)*5
+		self.velocity = (np.random.rand(3)-0.5)*7
 
 
 	# Take a timestep forward
 	def movecell(self,vel_change):
 		self.coords += self.velocity
 		self.velocity += ((np.random.rand(3)*2-1)*vel_change)/(np.linalg.norm(self.velocity)*2)
+		self.age += 1
 
 
 if __name__ == '__main__':
@@ -106,23 +119,25 @@ if __name__ == '__main__':
 		
 		image = np.zeros((img_size,img_size),dtype=np.uint8)
 		for cell in cells:
-			add_gaussian(image,cell.coords)
+			add_gaussian(image,cell.coords,cell.age)
 			if np.random.rand(1) < death*(pop/pop_target) or check_cell_bounds(cell):
 				remove.add(cell)
-			if np.random.rand(1) < split*(pop_target/pop):
+			#if np.random.rand(1) < split*(pop_target/pop):
+			cell.movecell(dv)
+			if cell.age == split_age:
+				cell.age = 0
 				add.add(cell)
-			cell.movecell(dv)		
 		cells = (cells - remove)
 
 		for cell in add:
-			cells.add(Cell(cell.coords+1,cell.lineage,unique_id))
+			cells.add(Cell(cell.coords+1,cell.lineage,unique_id,age=0))
 			unique_id += 1
 
 		if img is None:
 			img = plt.imshow(image)
 		else:
 			img.set_data(image)		
-		plt.pause(.05)
+		plt.pause(.5)
 		plt.draw()
 
 	
